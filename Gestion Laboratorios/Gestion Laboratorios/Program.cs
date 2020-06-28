6f70a831608a633c563660ade2a3a87760dbbc85
Aaron Martinez <aaronmartinez01@outlook.com>
2020-06-28 09:22:35 -0400

    Change home

--- Gestion Laboratorios/Gestion Laboratorios/Program.cs
+++ Gestion Laboratorios/Gestion Laboratorios/Program.cs
@@ -16,7 +16,7 @@ namespace Gestion_Laboratorios
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmGestionEmp());
+            Application.Run(new PantPrincipal());
         }
     }
 }
